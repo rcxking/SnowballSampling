0a0fd72a63813a168bf3eb6c9c6938e7fc068f0d
Bryant <rcxking@yahoo.com>
2012-09-23 18:47:31 -0400

    Added documentation for livejournal.py

--- livejournal.py
+++ livejournal.py
@@ -4,11 +4,25 @@ Access the LiveJournal API
 @author: bryant
 '''
 
+'''
+This is a Python Wrapper for the LiveJournal Online API
+'''
+
+# Import the built-in urllib library to access online URL's
 import urllib 
 
+'''
+Arguments:
+g is a Graph Data Structure (may be filled or empty)
+name is a string of the user to query for
+'''
+
 def read_lj_friends(g, name):
+	# The LJ API returns a JSON string with the user's data.  Store this
+    # into the variable "response"
     response = urllib.urlopen('http://www.livejournal.com/misc/fdata.bml?user='+name)
     
+	# We need to parse the JSON string to extract data
     for line in response.readlines():
         if line.startswith('#'): continue
         
